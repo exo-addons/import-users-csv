1471e6d9528c96026fafce3fca33f8c7c60aab09
mkrout@exoplatform.com <amineexo2012>
2016-10-20 16:13:06 +0100

    Add impot page to the administration Menu

---
+++ exo-addons-importUsersFromCSV-service/src/main/java/org/exoplatform/extension/importUsersFromCSV/UserBean.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright (C) 2003-2014 eXo Platform SAS.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.exoplatform.extension.importUsersFromCSV;
+
+/**
+ * Created by MedAmine.
+ */
+public class UserBean {
+
+
+    private String userName;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String groups;
+    private String spaces;
+
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setGroups(String groups) {
+        this.groups = groups;
+    }
+
+    public String getGroups() {
+        return groups;
+    }
+
+    public void setSpaces(String spaces) {
+        this.spaces = spaces;
+    }
+
+
+    public String getSpaces() {
+        return spaces;
+    }
+}
